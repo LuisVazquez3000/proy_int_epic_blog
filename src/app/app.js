e63c39aa884624eb1709b9d4463a0353f93098b3
LuisVazquez3000 <luis.vazquez.r25@gmail.com>
2023-09-13 00:35:05 -0300

    se crearon endpoints a utilizar

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const morgan = require("morgan");
 const router = require("../router/posts.router");
 
 const app = express();
 
+app.use(morgan("dev"));
 
 
 
--- src/router/posts.router.js
+++ src/router/posts.router.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
 
 
+router.get("/posts/{id}",(req, res)=>{
+	res.send("this is a router");
+} )
 router.get("/posts",(req, res)=>{
+	res.send("this is a router");
+} )
+router.post("/posts",(req, res)=>{
+	res.send("this is a router");
+} )
+router.put("/posts",(req, res)=>{
+	res.send("this is a router");
+} )
+router.delete("/posts",(req, res)=>{
 	res.send("this is a router");
 } )
 
